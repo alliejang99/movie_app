e54cfe4ef2c16fcd8e26204159a8e7e68bcc7f61
jangar6 <69830149+jangar6@users.noreply.github.com>
2021-02-22 15:41:06 +0900

    2021.02.22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  state = {
+    count: 0
+  };
+  add= () => {
+    this.setState(current => ({ count: current.count + 1}));
+  }
+  minus= () => {
+    this.setState(current => ({ count: current.count - 1}));
+  }
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just update!");
+  }
+  componentWillUnmount(){
+    console.log("Good bye Cruel world");
+  }
+  /*
+  실행 - RenderMount
+  setState - RenderUpdate
+  페이지 나갈 때 - WillUmnount
+  */
+  render(){
+    console.log("I am renderring");
+
+    return (
+    <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
